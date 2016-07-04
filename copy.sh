2c3b33bed5cd873eb3d214f6fda4889925c186f3
rongyi <sqrongyi@163.com>
2016-07-04 09:11:00 +0800

    Update copy shell

--- copy.sh
+++ copy.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
-cp ~/.emacs.d/init.el .
-cp ~/.emacs.d/elisp/* elisp/
-echo "copy current setting to here done"
+
+if [[ $# -gt 0 ]]
+then
+    cp init.el ~/.emacs.d/
+    cp -r elisp/rongyi-basic.el ~/.emacs.d/
+    cp -r elisp/rongyi-defun.el ~/.emacs.d/
+    echo "copy repo to emacs.d done"
+else
+    cp ~/.emacs.d/init.el .
+    cp ~/.emacs.d/elisp/* elisp/
+    echo "copy current setting to here done"
+fi
