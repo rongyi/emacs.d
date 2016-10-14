80d0c176dec91fd24eb446b40667eaf7fe4573e0
rongyi <sqrongyi@163.com>
2016-10-14 16:07:23 +0800

    Add snippet

--- cp_here.sh
+++ cp_here.sh
@@ -5,7 +5,7 @@ if [[ $# -gt 0 ]]
 then
     cp init.el ~/.emacs.d/
     cp -r elisp/* ~/.emacs.d/elisp/
-    echo "copy repo to emacs.d done"
+    echo "copy repo to .emacs.d done"
 else
     cp ~/.emacs.d/init.el .
     cp ~/.emacs.d/elisp/* elisp/
