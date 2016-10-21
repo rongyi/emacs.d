ddbab461db513a2407acbfd376a0a9dd3611ad5f
rongyi <sqrongyi@163.com>
2016-10-21 11:27:00 +0800

    Update script

--- cp_here.sh
+++ cp_here.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 
-
+# reverse action
 if [[ $# -gt 0 ]]
 then
     cp init.el ~/.emacs.d/
     cp -r elisp/* ~/.emacs.d/elisp/
-    echo "copy repo to .emacs.d done"
-else
+    cp -r ./snippets ~/.emacs.d/snippets
+    echo "sync repo to .emacs.d done"
+else # backup action
     cp ~/.emacs.d/init.el .
     cp ~/.emacs.d/elisp/* elisp/
-    echo "copy current setting to here done"
+    cp -r ~/.emacs.d/snippets/* ./snippets
+    echo "copy local setting to repo done"
 fi
