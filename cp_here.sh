940f2400bbdff0620f2c663a80738e4d4296558a
rongyi <sqrongyi@163.com>
2019-07-11 11:08:09 +0800

    Add ycmd global_conf, steal from spacemacs

--- cp_here.sh
+++ cp_here.sh
@@ -6,6 +6,8 @@ then                            # overwrite local settings! for install new mach
     mkdir -p ~/.emacs.d/elisp
     cp init.el ~/.emacs.d/
     cp -r elisp/* ~/.emacs.d/elisp/
+    # ycmd global_conf file
+    cp ./global_conf.py  ~/.emacs.d/
     echo "sync . ==> .emacs.d done"
 else # sync local to this repo
     cp ~/.emacs.d/init.el .
